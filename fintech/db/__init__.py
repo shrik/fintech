c68c32e85bc215ba055bde9c32aa97c9e1252e67
yuchao ma <yuchaoma@mayuchaos-Mac.local>
2017-06-14 10:54:54 +0800

    xx

--- fintech/db/__init__.py
+++ fintech/db/__init__.py
@@ -1,7 +1,8 @@
 import sqlalchemy
-engine = sqlalchemy.create_engine('mysql://root:@localhost/fintech?charset=utf8', echo=True)
+engine = sqlalchemy.create_engine('mysql+pymysql://root:@localhost/fintech?charset=utf8', echo=True)
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
 from hibor_doc import HiborDoc
+from doctext import Doctext
--- fintech/etl/parse_doc_to_text.py
+++ fintech/etl/parse_doc_to_text.py
@@ -8,28 +8,25 @@ import commands
 import json
 
    
-def set_hibor_doc_status_to_parsed(hibor_doc):
-    hibor_doc.values(data_status="parsed")
 
 def parse(hibor_doc):
     prefix = "/data/money/fintech/hibor_doc"
     doc_path = ("%06d" % hibor_doc.id)
-    path = ("/").join([id_path[i:i+2] for i in range(0, len(doc_path), 2)])
+    path = ("/").join([doc_path[i:i+2] for i in range(0, len(doc_path), 2)])
     file_path = prefix + "/" + path + ".pdf"
-    content = commands.getoutput("java -jar -Dfile.encoding=utf-8 /home/spark/tabula-0.9.2-jar-with-dependencies.jar -p all -g " % file_path)
+    content = commands.getoutput("java -jar -Dfile.encoding=utf-8 /home/spark/tabula-0.9.2-jar-with-dependencies.jar -p all -g %s " % file_path)
     # content = commands.getoutput("echo I Love You")
     # content = "java -jar -Dfile.encoding=utf-8 /home/spark/tabula-0.9.2-jar-with-dependencies.jar -p all -g " % file_path
     return content
 
-def new_doc(hibor_doc, content):
+def new_doctext(hibor_doc, content):
     return db.Doctext(hibor_doc_id=hibor_doc.id, content=content)
 
 
-hibor_docs = db.session.query(db.HiborDoc).filter_by(data_status = "downloaded", filetype= ".pdf").first()
+hibor_docs = db.session.query(db.HiborDoc).filter_by(data_status = "downloaded", filetype= ".pdf").all()
 for hibor_doc in hibor_docs:
-    hibor_doc = db.session.query(db.HiborDoc).filter_by(hibor_doc_id=hibor_doc.id).first()
-    set_hibor_doc_status_to_parsed(hibor_doc)    
-    cotent = parse(hibor_doc)
+    hibor_doc.data_status = "parsed"
+    content = parse(hibor_doc)
     doctext = new_doctext(hibor_doc, content)
     db.session.add(hibor_doc)
     db.session.add(doctext)
